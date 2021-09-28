0f546a73a93df2f70242c74899639acd2b5b7ba8
Pv Duc <pvduc.dev@gmail.com>
2021-09-28 23:36:44 +0700

    Initial commit

--- src/components/pages/MapPage.tsx
+++ src/components/pages/MapPage.tsx
@@ -1,64 +1,31 @@
 import React, {useState} from 'react';
-import ReactMapGL, {
-  AttributionControl,
-  FullscreenControl,
-  Marker,
-  NavigationControl,
-  ViewportProps,
-  ViewState,
-  CanvasOverlay, GeolocateControl
-} from 'react-map-gl';
+import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+interface UrlParams {
+  latitude?: string;
+  longitude?: string;
+}
 
 const MapPage = () => {
   const mapboxAccessToken = 'pk.eyJ1IjoicHZkdWMtZGV2IiwiYSI6ImNrOHlid2lkdDFnZnUzam52cGFwOHBqcGMifQ.PFeqdmk4RuusDS3GruLvEA';
-
-  const [viewport, setViewport] = useState<ViewportProps>({
-    latitude: 37.7577,
-    longitude: -122.4376,
-    zoom: 8,
-  });
-
-  const viewportChangeHandler = (viewport: ViewportProps) => {
-    setViewport(viewport)
-  }
-
-  const fullscreenControlStyle = {
-    right: 10,
-    top: 10,
-  };
+  const Map = ReactMapboxGl({
+    accessToken: "pk.eyJ1IjoicHZkdWMtZGV2IiwiYSI6ImNrOHlid2lkdDFnZnUzam52cGFwOHBqcGMifQ.PFeqdmk4RuusDS3GruLvEA",
+    logoPosition: 'top-left'
+  })
   return (
-    <div
-      style={
-        {width: '100vw', height: '100vh'}
-      }
+    <Map
+      style="mapbox://styles/mapbox/streets-v11"
+      containerStyle={{
+        height: '100vh',
+        width: '100vw'
+      }}
     >
-      <ReactMapGL
-        {...viewport}
-        mapStyle={'mapbox://styles/mapbox/light-v10'}
-        width="100%"
-        height="100%"
-        mapboxApiAccessToken={mapboxAccessToken}
-        onViewportChange={viewportChangeHandler}
-      >
-        <Marker latitude={37.78} longitude={-122.41} offsetLeft={-20} offsetTop={-10}>
-        </Marker>
-        <NavigationControl
-          style={{top: 50, right: 10}}
-        />
-        <FullscreenControl
-          style={{top: 10, right: 10}}
-        >
-          test
-        </FullscreenControl>
-        <GeolocateControl
-          style={{bottom: 28, right: 10}}
-          positionOptions={{enableHighAccuracy: true}}
-          trackUserLocation={true}
-          auto
-        />
-      </ReactMapGL>
-    </div>
-  );
+      <Layer type="symbol" id="marker" layout={{ 'icon-image': 'marker-15' }}>
+        <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
+      </Layer>
+    </Map>
+  )
 };
 
 export default MapPage;
