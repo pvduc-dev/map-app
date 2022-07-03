8b9a39e9935bf80638e8716c89a4d534522cc63e
pvduc <pvduc.dev@gmail.com>
2022-07-03 18:49:42 +0700

    feat: add new UI

--- src/App.tsx
+++ src/App.tsx
@@ -2,47 +2,34 @@ import React, {FC} from 'react';
 import Routes from "./components/routes";
 import {BrowserRouter as Router, Redirect, Route, Switch} from "react-router-dom";
 import './style/index.css';
-import { NotifierContextProvider } from 'react-headless-notifier';
-import {
-  useQuery,
-  useMutation,
-  useQueryClient,
-  QueryClient,
-  QueryClientProvider,
-} from 'react-query'
-import { ReactQueryDevtools } from 'react-query/devtools'
-
-const queryClient = new QueryClient();
+import {NotifierContextProvider} from 'react-headless-notifier';
 
 const App: FC = () => (
-  <QueryClientProvider client={queryClient}>
-    <ReactQueryDevtools initialIsOpen={false} />
-    <NotifierContextProvider
-      config={{
-        max: 3,
-        duration: 200000,
-        position: 'bottom',
-      }}
+  <NotifierContextProvider
+    config={{
+      max: 3,
+      duration: 200000,
+      position: 'bottom',
+    }}
+  >
+    <div
+      className="relative w-full h-screen overflow-x-hidden"
     >
-      <div
-        className="relative w-full h-screen overflow-x-hidden"
-      >
-        <Router>
-          <Switch>
-            <Route
-              path="/map"
-            >
-              <Routes/>
-            </Route>
-            <Redirect
-              to="/map"
-              from="/"
-            />
-          </Switch>
-        </Router>
-      </div>
-    </NotifierContextProvider>
-  </QueryClientProvider>
+      <Router>
+        <Switch>
+          <Route
+            path="/map"
+          >
+            <Routes/>
+          </Route>
+          <Redirect
+            to="/map"
+            from="/"
+          />
+        </Switch>
+      </Router>
+    </div>
+  </NotifierContextProvider>
 )
 
 export default App;
